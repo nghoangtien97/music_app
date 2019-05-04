fa958bd72fce53da1fe1a2e926fa8cc943531b1a
nghoangtien97 <nghoangtien97@gmail.com>
2019-05-04 17:27:41 +0700

    Add some onclicklistener and todo in playing_activity

--- app/src/main/java/assignment/mobile/playmp3/PlayingActivity.java
+++ app/src/main/java/assignment/mobile/playmp3/PlayingActivity.java
@@ -39,19 +39,76 @@ public class PlayingActivity extends AppCompatActivity  {
         _playing.setOnClickListener((View v) -> {
             is_playing = !is_playing;
             if (is_playing) {
-                _playing.setImageResource(R.drawable.playing_play);
+                // TODO: continue playing song.
+
+                _playing.setImageResource(R.drawable.playing_play_enable);
             } else {
-                _playing.setImageResource(R.drawable.playing_stop);
+                // TODO: stop playing.
+
+                _playing.setImageResource(R.drawable.playing_stop_enable);
             }
         });
 
         _playing_shuffle.setOnClickListener((View v) -> {
             is_shuffled = !is_shuffled;
             if (is_shuffled) {
+                // TODO: random playlist
+
                 _playing_shuffle.setImageResource(R.drawable.playing_shuffle_enable);
             } else {
+                // TODO: disable random
+
                 _playing_shuffle.setImageResource(R.drawable.playing_shuffle);
             }
+        });
+
+        _playing_repeat.setOnClickListener((View v) -> {
+            // 0: disable, 1: enable (repeat list), 2: repeat one
+            is_repeated = (++is_repeated) % 3;
+            switch (is_repeated) {
+                // TODO: disable repeated
+                case 0: {
+
+                    _playing_repeat.setImageResource(R.drawable.playing_repeat);
+                    break;
+                }
+                // TODO: repeat all list
+                case 1: {
+
+                    _playing_repeat.setImageResource(R.drawable.playing_repeat_enable);
+                    break;
+                }
+                // TODO: repeat current song
+                case 2: {
+
+                    _playing_repeat.setImageResource(R.drawable.playing_repeat_one);
+                    break;
+                }
+                default: {
+                }
+            }
+        });
+
+        _playing_pre.setOnClickListener((View v) -> {
+            // TODO: play previous song in list
+            // Change name_song, name_author
+
+        });
+
+        _playing_next.setOnClickListener((View v) -> {
+            // TODO: play next song in list
+            // Change name_song, name_author
+            
+        });
+
+        _btn_back.setOnClickListener((View v) -> {
+            // Back to previous activity
+            finish();
+        });
+
+        _btn_add.setOnClickListener((View v) -> {
+            // TODO: Add current song to library
+
         });
     }
 }
