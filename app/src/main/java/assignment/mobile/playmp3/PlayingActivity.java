1622b6f664780a06593dd1b39b7df1a23862b026
nghoangtien97 <nghoangtien97@gmail.com>
2019-05-04 18:29:48 +0700

    Add slide views to show main/lyrics of current song

--- app/src/main/java/assignment/mobile/playmp3/PlayingActivity.java
+++ app/src/main/java/assignment/mobile/playmp3/PlayingActivity.java
@@ -1,9 +1,17 @@
 package assignment.mobile.playmp3;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PlayingActivity extends AppCompatActivity  {
@@ -21,6 +29,12 @@ public class PlayingActivity extends AppCompatActivity  {
     TextView _name_song;
     TextView _name_author;
 
+    ViewPager _view_pager;
+    IntroViewPagerAdapter _pager_adapter;
+    LinearLayout _dots_layout;
+    TextView[] _dots;
+    int[] _layouts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +49,14 @@ public class PlayingActivity extends AppCompatActivity  {
         _btn_add = findViewById(R.id.btn_add);
         _name_song = findViewById(R.id.name_song);
         _name_author = findViewById(R.id.name_author);
+
+        _view_pager = findViewById(R.id.view_pager);
+        _dots_layout = findViewById(R.id.layout_dots);
+        _layouts = new int[]{R.layout.playing_main, R.layout.playing_lyrics};
+        addBottomDots(0);
+        _pager_adapter = new IntroViewPagerAdapter();
+        _view_pager.setAdapter(_pager_adapter);
+        _view_pager.addOnPageChangeListener(mViewPagerChangeListener);
 
         _playing.setOnClickListener((View v) -> {
             is_playing = !is_playing;
@@ -98,7 +120,7 @@ public class PlayingActivity extends AppCompatActivity  {
         _playing_next.setOnClickListener((View v) -> {
             // TODO: play next song in list
             // Change name_song, name_author
-            
+
         });
 
         _btn_back.setOnClickListener((View v) -> {
@@ -111,4 +133,83 @@ public class PlayingActivity extends AppCompatActivity  {
 
         });
     }
+
+
+    // TODO: Implement methods here
+
+
+
+
+
+
+
+    private ViewPager.OnPageChangeListener mViewPagerChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private void addBottomDots(int currentPage) {
+        _dots = new TextView[_layouts.length];
+        _dots_layout.removeAllViews();
+
+        int normalColor = getResources().getColor(R.color.white);
+        int enableColor = getResources().getColor(R.color.enable);
+
+        for (int i = 0; i < _dots.length; i++) {
+            _dots[i] = new TextView(this);
+            _dots[i].setText(Html.fromHtml("."));
+            _dots[i].setTextSize(35);
+            _dots[i].setTextColor(normalColor);
+            _dots_layout.addView(_dots[i]);
+        }
+        if (_dots.length > 0) {
+            _dots[currentPage].setTextColor(enableColor);
+        }
+    }
+
+    public class IntroViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater inflater;
+
+        public IntroViewPagerAdapter() {
+            super();
+        }
+
+        @Override
+        @NonNull
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(_layouts[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return _layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view,@NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position,@NonNull Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
 }
+
